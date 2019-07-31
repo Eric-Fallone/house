eba2f7dfbba9a266ac9b4e171f046fd2cef5b7fd
TigerPup <ericfallone@gmail.com>
2019-07-30 22:28:56 -0500

    Added Pages and Hooked up nav bar links to said pages
    Also Added nav bar picture

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@ var express = require("express");
 var router  = express.Router();
 
 router.get("/", function(req,res){
-  res.render("landing");
+  res.render("Landing");
 });
 
+router.get("/About", function(req,res){
+  res.render("About");
+});
+
+router.get("/Contact", function(req,res){
+  res.render("Contact");
+});
 module.exports = router;
