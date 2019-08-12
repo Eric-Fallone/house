4d34f5ffd17b5a6f7809f62166d715650f0617a5
TigerPup <ericfallone@gmail.com>
2019-08-11 19:47:30 -0500

    Header set highlight when on the correct page

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,27 @@ var express = require("express");
 var router  = express.Router();
 
 router.get("/", function(req,res){
+  res.locals.page="Landing";
   res.render("Landing");
 });
 
 router.get("/OurHomes", function(req,res){
+  res.locals.page="OurHomes";
   res.render("OurHomes");
 });
 
 router.get("/About", function(req,res){
+  res.locals.page="About";
   res.render("About");
 });
 
 router.get("/Gallery", function(req,res){
+  res.locals.page="Gallery";
   res.render("Gallery");
 });
 
 router.get("/Contact", function(req,res){
+  res.locals.page="Contact";
   res.render("Contact");
 });
 
